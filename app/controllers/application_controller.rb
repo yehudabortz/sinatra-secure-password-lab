b261eea5fed950c5048acb684adcb1cde218036c
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-31 13:16:39 -0600

    commit tree ed140128d743c4c7f8649c12cb70a76ef6e55e05

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,22 +17,31 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
-  end
+		if params[:username] == "" || params[:password] == ""
+      redirect "/failure"
+		else
+      User.create(username: params[:username], password: params[:password])
+      redirect "/login"
+		end
+	end
 
   get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
+    @user = current_user
+      erb :account
   end
-
 
   get "/login" do
     erb :login
   end
 
   post "/login" do
-    ##your code here
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      redirect '/failure'
+    end
   end
 
   get "/failure" do
